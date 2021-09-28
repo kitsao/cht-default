cde27943467828829c4aeb0074eb54bfdde3cfd3
kitsao <kittemm@gmail.com>
2021-09-28 16:08:39 +0300

    more updates

--- .github/actions/update-rapidpro-workspace-settings/index.js
+++ .github/actions/update-rapidpro-workspace-settings/index.js
@@ -20,13 +20,13 @@ try {
   }
   const codeRepository = path.resolve(path.resolve(githubWorkspacePath), core.getInput('directory'));
   process.chdir(codeRepository);
-  const appSettings = require('./app_settings.json');
+  const appSettings = require(`${codeRepository}/app_settings.json`);
   const rp_hostname = core.getInput('rp_hostname');
   const value_key = core.getInput('value_key');
   const rp_contact_group = core.getInput('rp_contact_group');
   const write_patient_state_flow = core.getInput('write_patient_state_flow');
   const options = {
-    files: './app_settings.json',
+    files: codeRepository+'/app_settings.json',
     from: [regex(search(appSettings, 'base_url')), regex(search(appSettings, 'value_key')), search(appSettings, 'groups').expr, search(appSettings, 'flow').expr],
     to: [rp_hostname, value_key, `['${rp_contact_group}']`, `'${write_patient_state_flow}'`]
   };
