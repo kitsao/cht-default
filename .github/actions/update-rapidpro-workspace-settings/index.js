943ca6bf1c3f2aded747d750c3000dded51b6d2e
kitsao <kittemm@gmail.com>
2021-09-29 11:18:12 +0300

    axios

--- .github/actions/update-rapidpro-workspace-settings/index.js
+++ .github/actions/update-rapidpro-workspace-settings/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const replace = require('replace-in-file');
 const path = require('path');
-const recursive = require('recursive-readdir');
+const axios = require('axios').default;
 
 const search = (haystack, needle) => needle in haystack ? haystack[needle] : Object.values(haystack).reduce((acc, val) => {
   if (acc !== undefined) {
@@ -13,18 +13,35 @@ const search = (haystack, needle) => needle in haystack ? haystack[needle] : Obj
 
 const regex = expr => new RegExp(expr, 'g');
 
+const setMedicCredentials = (couch_username, couch_password, hostname, couch_node_name, rp_api_token) => {
+  axios({
+    method: 'put',
+    url: `https://${couch_username}:${couch_password}@${hostname}/_node/${couch_node_name}/_config/medic-credentials/${value_key}`,
+    data: `"${rp_api_token}"`
+  });
+};
+
 try {
-  const githubWorkspacePath = process.env['GITHUB_WORKSPACE']
+
+
+  const githubWorkspacePath = process.env['GITHUB_WORKSPACE'];
+  const rp_hostname = core.getInput('rp_hostname');
+  const value_key = core.getInput('value_key');
+  const rp_contact_group = core.getInput('rp_contact_group');
+  const write_patient_state_flow = core.getInput('write_patient_state_flow');
+  const couch_username = core.getInput('couch_username');
+  const couch_password = core.getInput('couch_password');
+  const hostname = core.getInput('hostname');
+  const couch_node_name = core.getInput('couch_node_name');
+  const rp_api_token = core.getInput('rp_api_token');
+  // Update medic core
+  setMedicCredentials(couch_username, couch_password, hostname, couch_node_name, rp_api_token);
   if (!githubWorkspacePath) {
     throw new Error('GITHUB_WORKSPACE not defined')
   }
   const codeRepository = path.resolve(path.resolve(githubWorkspacePath), core.getInput('directory'));
   process.chdir(codeRepository);
   const appSettings = require(`${codeRepository}/app_settings.json`);
-  const rp_hostname = core.getInput('rp_hostname');
-  const value_key = core.getInput('value_key');
-  const rp_contact_group = core.getInput('rp_contact_group');
-  const write_patient_state_flow = core.getInput('write_patient_state_flow');
   const options = {
     files: codeRepository+'/app_settings.json',
     from: [regex(search(appSettings, 'base_url')), regex(search(appSettings, 'value_key')), search(appSettings, 'groups').expr, search(appSettings, 'flow').expr],
